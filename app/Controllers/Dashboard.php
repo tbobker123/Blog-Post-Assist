2cfa5169476da8ea93674c0f2bc2aae34233c99a
Tim Bobker <tim@domaingenerator.app>
2022-10-11 17:25:16 +0100

    Clean up controller

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -34,36 +34,6 @@ class Dashboard extends BaseController
         //echo "<pre>"; print_r($data); echo "</pre>"; exit;
         return view('dashboard', $data);
     }
-
-    public function saveSERPResults(){
-
-		if( $this->request->getMethod() == "post" AND $this->request->getPost('save_serps') ){
-			
-			$query = $this->request->getPost('query');
-			$results = $this->request->getPost('results');
-			$relatedquestions = $this->request->getPost('relatedquestions');
-			$wordcount = $this->request->getPost('wordcount');
-
-			$data = [
-				'query'=> $query,
-				'results' => $results,
-				'relatedquestions' => $relatedquestions,
-				'wordcount' => $wordcount,
-			];
-
-			$this->saveSERP->update($data);
-
-			return $this->response([
-                'query' => $query,
-                'saved' => 'true'
-            ]);
-
-		} else {
-            return $this->response([
-                'error' => 'failure'
-            ]);
-        }
-	}
 }
 
 
